1659e1fb6063d41c885d5657f5541d17e79c8c0e
kanikanaik <kanikalnaik@gmail.com>
2025-08-05 20:41:10 +0530

    Some changes

---
+++ Arrays/Sliding_window/maxsubarray.cpp
@@ -0,0 +1,15 @@
+// Find maximum (or minimum) sum of a subarray of size k
+
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {100, 200, 300, 400};
+    int k = 2;
+
+    
+}
