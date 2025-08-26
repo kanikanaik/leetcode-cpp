16eaa5fab84598aa1a3b957b93360432aa615db6
kanikanaik <kanikalnaik@gmail.com>
2025-08-26 19:31:17 +0530

    1652. Defuse the Bomb

---
+++ Arrays/Sliding_window/decryptBomb.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> decrypt(vector<int> &code, int k)
+{
+    vector<int> res(code.size(), 0);
+    int N = code.size();
+
+    if (k == 0)
+    {
+        return res;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        if (k > 0)
+        {
+            for (int j = i + 1; j < i + 1 + k; j++)
+            {
+                res[i] += code[j % N];
+            }
+        }
+        else
+        {
+            for (int j = i - 1; j > i - 1 - abs(k); j--)
+            {
+                res[i] += code[((j % N) + N) % N];
+            }
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    vector<int> code = {2, 4, 9, 3};
+    int k = -2;
+
+    vector<int> res = decrypt(code, k);
+
+    for (int i : res)
+    {
+        cout << i << " ";
+    }
+}
--- Arrays/Sliding_window/maxsubarray.cpp
+++ Arrays/Sliding_window/maxsubarray.cpp
@@ -8,9 +8,23 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr = {3,8,2,5,7,6,12};
+    vector<int> arr = {3, 8, 2, 5,7, 6, 12};
     int k = 4;
-    
 
-    
+    int current = 0;
+    for (int i = 0; i < k; i++)
+    {
+        current += arr[i];
+    }
+
+    int maxx = current;
+    for (int i = 1; i < arr.size() - k; i++)
+    {
+        current = current - arr[i - 1] + arr[i + k - 1];
+        if (current > maxx)
+        {
+            maxx = current;
+        }
+    }
+    cout << maxx << " ";
 }
