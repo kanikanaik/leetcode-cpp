ada1af8629442fed122618d73ae0fb0013cfad8e
kanikanaik <kanikalnaik@gmail.com>
2025-09-07 23:18:56 +0530

    94. Binary Tree Inorder Traversal

---
+++ BinarySearchTree/in-leet.cpp
@@ -0,0 +1,39 @@
+// 94. Binary Tree Inorder Traversal
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+//   Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<int> inorderTraversal(TreeNode *root)
+{
+    vector<int> res;
+    traversal(root, res);
+    return res;
+}
+
+void traversal(TreeNode *root, vector<int> res)
+{
+    if (root != NULL)
+    {
+        traversal(root->left, res);
+        // cout << root-> data << " ";
+        res.push_back(root->val);
+        traversal(root->right, res);
+    }
+}
+int main()
+{
+    vector<int> root = {1, -1, 2, 3};
+}
