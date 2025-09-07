2ac367a5746131594b369e9a7e85846c1d4a0e15
kanikanaik <kanikalnaik@gmail.com>
2025-09-07 23:31:29 +0530

    Traversals

---
+++ BinarySearchTree/post-leet.cpp
@@ -0,0 +1,40 @@
+// 144. Binary Tree Preorder Traversal
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<int> postorderTraversal(TreeNode *root)
+{
+    vector<int> res;
+    traversal(root, res);
+    return res;
+}
+
+void traversal(TreeNode *root, vector<int> &res)
+{
+    if (root != NULL)
+    {
+        
+        traversal(root->left,res);
+        traversal(root->right,res);
+        res.push_back(root->val);
+    }
+}
+
+int main()
+{
+    vector<int> root = {1, -1, 2, 3};
+    // for (int i : preorderTraversal(root))
+}
