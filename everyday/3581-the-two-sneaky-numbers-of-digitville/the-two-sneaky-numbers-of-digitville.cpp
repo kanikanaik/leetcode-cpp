bae53f36e56c71ce94e99001480b7ea18aca17bd
Kanika Naik <94538162+kanikanaik@users.noreply.github.com>
2025-11-01 12:29:07 +0530

    Time: 0 ms (100.00%) | Memory: 25.4 MB (75.74%) - LeetSync

--- everyday/3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ everyday/3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        vector<int> res;
-        unordered_map<int, int> count;
+        int XOR = 0;
+        int n = nums.size() - 2;
 
+        for (int num : nums) XOR ^= num;            
+        for (int i = 0; i < n; i++) XOR ^= i;
+        //for (int i = 0; i < n; i++) XOR ^= nums[i] ^ (i < n ? i : 0); //single-pass
+
+        int diffBit = XOR & -XOR;
+
+        int a = 0, b = 0;
         for (int num : nums) {
-            count[num]++;
+            if ((num & diffBit) == 0) a ^= num;
+            else b ^= num;
+        }
+        for (int i = 0; i < n; i++) {
+            if ((i & diffBit) == 0) a ^= i;
+            else b ^= i;
         }
 
-        for (auto i : count) {
-            if (i.second >= 2) {
-                res.push_back(i.first);
-                // cout << i.first << " : " << i.second << endl;
-            }
-        }
-
-        return res;
+        return {a, b};
     }
 };
