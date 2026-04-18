c4a4f9cbaf5825228e1f86f0cf988c20062707fb
Kanika Naik <94538162+kanikanaik@users.noreply.github.com>
2026-04-18 17:39:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (85.48%) - LeetSync

--- everyday/70-climbing-stairs/climbing-stairs.cpp
+++ everyday/70-climbing-stairs/climbing-stairs.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-
+        // vector<int> dp(n + 1, -1);
+        if(n <= 1) return n;
+        int prev, prev2, curr;
         // Base cases
-        dp[0] = 1;
-        dp[1] = 1;
+       prev2 = 1;
+        prev = 1;
 
         // Fill dp array using bottom-up dynamic programming
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            curr = prev + prev2;
+            prev2 = prev;
+            prev =curr;
         }
 
         // Print the nth Fibonacci number
-       return dp[n];
+       return prev;
     }
 };
