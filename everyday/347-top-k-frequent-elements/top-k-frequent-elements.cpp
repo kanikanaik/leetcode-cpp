2c8c6b416fea1599c6e2d1b587707133a74ce68f
Kanika Naik <94538162+kanikanaik@users.noreply.github.com>
2026-04-25 18:17:10 +0530

    Time: 8 ms (8.84%) | Memory: 18.1 MB (38.55%) - LeetSync

--- everyday/347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ everyday/347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -8,7 +8,7 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             mp[nums[i]]++;
         }
-         priority_queue<pair<int, int>> pq;
+        priority_queue<pair<int, int>> pq;
 
         for (auto it : mp) {
             pq.push({it.second, it.first});
@@ -17,7 +17,7 @@ public:
         vector<int> ans;
 
         // Get top k elements
-        while (k--) {
+        for (int i = 0; i < k; i++) {
             ans.push_back(pq.top().second);
             pq.pop();
         }
