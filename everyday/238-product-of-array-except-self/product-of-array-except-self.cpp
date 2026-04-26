2ddcad13ea2c5145c78e1a1cdfcd53f6d08a9354
Kanika Naik <94538162+kanikanaik@users.noreply.github.com>
2026-04-26 13:31:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (24.89%) - LeetSync

---
+++ everyday/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n,1);
+        vector<int> suffix(n,1);
+        vector<int> ans(n);
+
+        
+        // prefix[0] = 1;
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i -1];
+        }
+
+        // suffix[n - 1] = nums[n -1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = prefix[i] * suffix[i];
+        }
+        return ans;
+    }
+};
+
+// 1,2,3,4
+// // 1,2,6,24
+// [1, 1, 2, 6]
+// [24, 12, 4, 1]
+// i = 0, j =0
