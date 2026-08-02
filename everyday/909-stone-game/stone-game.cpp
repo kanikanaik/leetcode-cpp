b2a22d825df75ecd852bcda8fb353b2bb3b2cb9c
Kanika Naik <94538162+kanikanaik@users.noreply.github.com>
2026-08-03 00:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.24%) - LeetSync

---
+++ everyday/909-stone-game/stone-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // int alice = 0;
+        // int bob = 0;
+        // for(int i = 0 ; i < piles.size(); i++){
+        //     if(i % 2 == 0){
+        //         alice += piles[i];
+        //     }else{
+        //         bob += piles[i];
+        //     }
+        // }
+
+        // if(alice > bob){return true;}else{return false;}
+        return true;
+    }
+};
