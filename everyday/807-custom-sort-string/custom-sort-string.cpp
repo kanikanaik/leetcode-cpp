33225d0d882fd54e7c879ab508b894cefba4e6e6
Kanika Naik <94538162+kanikanaik@users.noreply.github.com>
2026-08-09 14:06:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (71.40%) - LeetSync

--- everyday/807-custom-sort-string/custom-sort-string.cpp
+++ everyday/807-custom-sort-string/custom-sort-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string customSortString(string order, string s)
+   string customSortString(string order, string s)
 {
     unordered_map<char, int> seq;
 
@@ -10,13 +10,30 @@ public:
     }
     // cout << s;
     // cout << seq['a'];
-    sort(s.begin(), s.end(), [&](char a, char b)
-         {
-        int priorityA = seq.count(a) ? seq[a] : order.length() + 1;
-        int priorityB = seq.count(b) ? seq[b] : order.length() + 1;
 
-        return priorityA < priorityB; });
-    return s;
+    string result = "";
+    for (int i = 0; i < order.length(); i++)
+    {
+        char c = order[i];
+        if (s.find(c) != string::npos)
+        {
+            for (char x : s)
+            {
+                if (x == c)
+                    result += c;
+            }
+        }
+    }
+
+    for (char c : s)
+    {
+        if (seq.find(c) == seq.end())
+        {
+            result += c;
+        }
+    }
+
+    return result;
 }
 };
 
